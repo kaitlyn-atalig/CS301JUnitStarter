3a1c1b81b922141b6fa2e9ae70d793802110f4d6
Bryan Schlief <schlief@up.edu>
2023-04-09 13:10:43 -0700

    Attempt to modernize the project

--- app/src/main/java/up/edu/dimcalc/DimCalc.java
+++ app/src/main/java/up/edu/dimcalc/DimCalc.java
@@ -1,11 +1,12 @@
 package up.edu.dimcalc;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class DimCalc extends AppCompatActivity implements View.OnClickListener {
 
